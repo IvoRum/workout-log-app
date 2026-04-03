fcaf1b599bc44a777f87563ebaa7142c39635b68
IRumenov <ivaylo.rumenov@unicard-uk.com>
2026-04-03 16:15:10 +0300

    managed to create a woking save and load method using json files and a tutorial in gd docs

--- scripts/exercise_list.gd
+++ scripts/exercise_list.gd
@@ -8,20 +8,36 @@ var exercise_data_path="res://data/Exercise.tres"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var load_file = FileAccess.open(exercise_data_path, FileAccess.READ)
+	var save_file = FileAccess.open(exercise_data_path, FileAccess.READ)
 	
-	var exercise = Excercise.new()
-	exercise.description = "description"
-	exercise.excercise_name = "push up"
-	print("Created new exercise")
-	
-	var tekst = Label.new()
-	tekst.text = exercise.excercise_name
-	v_box_container.add_child(tekst)
-	
-	exercises.append(exercise)
-	print("Appended list of exercises")
-	save_exercises()
+	# Load the file line by line and process that dictionary to restore
+	# the object it represents.
+	while save_file.get_position() < save_file.get_length():
+		var json_string = save_file.get_line()
+
+		# Creates the helper class to interact with JSON.
+		var json = JSON.new()
+
+		# Check if there is any error while parsing the JSON string, skip in case of failure.
+		var parse_result = json.parse(json_string)
+		if not parse_result == OK:
+			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+			continue
+
+		# Get the data from the JSON object.
+		var node_data = json.data
+		
+		var exercise = Excercise.new()
+		exercise.description = node_data["description"]
+		exercise.excercise_name = node_data["excercise_name"]
+		print("Created new exercise")
+		
+		var tekst = Label.new()
+		tekst.text = exercise.excercise_name
+		v_box_container.add_child(tekst)
+		
+		exercises.append(exercise)
+		print("Appended list of exercises")
 
 func save_exercises() -> void:
 	var save_file = FileAccess.open(exercise_data_path, FileAccess.WRITE)
@@ -44,3 +60,18 @@ func save_exercises() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func create_and_save_excerice_exe() -> void:
+	var exercise = Excercise.new()
+	exercise.description = "description"
+	exercise.excercise_name = "push up"
+	print("Created new exercise")
+	
+	var tekst = Label.new()
+	tekst.text = exercise.excercise_name
+	v_box_container.add_child(tekst)
+	
+	exercises.append(exercise)
+	print("Appended list of exercises")
+	save_exercises()
